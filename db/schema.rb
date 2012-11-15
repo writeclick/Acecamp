23c3713d64d0dcc709a478ce5b19f6710fcb84b2
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-15 11:40:04 -0800

    created projects model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121115193449) do
+ActiveRecord::Schema.define(:version => 20121115193701) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false

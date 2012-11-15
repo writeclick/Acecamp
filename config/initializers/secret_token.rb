0c5a53c2b029e9a16c596850697b249a243296e7
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-15 11:17:18 -0800

    Initializing new rails app called dbc_camp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DbcCamp::Application.config.secret_token = '6e366d6e34f90290db221b4d17f58bbf69f13d525e65df872fd7c373457f2fa431cd9f47234b5e1ddee587ab94d048d6edbd616b98109fb4634b302def10c24b'

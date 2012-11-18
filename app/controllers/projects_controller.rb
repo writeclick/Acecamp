5d13bbd26d736278221745c328557eb5d99225f1
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-18 15:23:21 -0800

    uncommented stuff for can-can implementation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,47 +1,63 @@
 class ProjectsController < ApplicationController
+	#load_and_authorize_resource
+	before_filter :load_user
+	before_filter :load_project, :except => [:new, :create, :index]	
 
 	def index
-		@user = User.find(current_user.id)
 		@projects = @user.projects
 		
+		@collaborating_projects = Collaborator.find_collaborations(@user.id)
 	end
 
 	def new
-		@user = User.find(params[:user_id])
 		@project = Project.new
 	end
 
 	def create
-		@user = User.find(params[:user_id])
 		@project = Project.new(params[:project])
 		@project.user_id = @user.id
 
 		if @project.save
-			redirect_to user_project_path(@user.id, @project.id)
+			redirect_to project_path(@project.id)
 		else
 			render :new
 		end
 	end
 
 	def show
-		@user = User.find(params[:user_id]) ||
-			User.find_by_email(params[:user][:email])
-		@project = Project.find(params[:id])
+		@user ||= User.find_by_email(params[:user][:email])
 		@collaborator = Collaborator.new
+
+		if @user.collaborator?(@project)
+			@temp = "Yo Dude!"
+		else
+			@temp = "You are NOT a collaborator"
+		end
+
+		if @user.owner?(@project)
+			@temp2 = "I am an owner!"
+		else
+			@temp2 = "I am NOT an owner"
+		end
+
 	end
 
 	def edit
-		@user = User.find(params[:user_id])
-		@project = Project.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:user_id])
-		@project = Project.find(params[:id])
 	end
 
 	def destroy
-		@user = User.find(params[:user_id])
+	end
+
+private
+
+	def load_user
+		@user = current_user
+	end
+
+	def load_project
 		@project = Project.find(params[:id])
 	end
 	

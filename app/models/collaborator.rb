639f99bde09c4a44227f241ab714d0757c413f2d
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-18 16:36:46 -0800

    got rid of can-can / debugging. really.

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -2,14 +2,19 @@ class Collaborator < ActiveRecord::Base
   attr_accessible :project_id, :user_id
 
   def self.find_collaborations(user_id)
-  	if Collaborator.any?
-  		collaborations = Collaborator.select(:project_id).where(:user_id => user_id)
+  	collaborations = Collaborator.select(:project_id).where(:user_id => user_id)
+  	if collaborations != []
 			collaborations.map! { |collaboration| collaboration = collaboration.project_id }
 			Project.find(collaborations)
 		else
-			[]
+			collaborations
 		end
   end
 
 
 end
+
+
+	
+
+

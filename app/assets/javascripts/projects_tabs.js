6c75a30cf8fbc400dce6c9073fcc6aa898857768
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-20 22:12:05 -0800

    major UI changes

--- app/assets/javascripts/projects_tabs.js
+++ app/assets/javascripts/projects_tabs.js
@@ -11,3 +11,10 @@ $('a[data-toggle="tab"]').on('shown', function(e) {
 	e.target
 	e.relatedTarget
 });
+
+//===================== accordion for projects ===============
+
+$('#accordion-projects').on('click', 'a', function(e){
+		$('.accordion-body').collapse('hide');
+		$(this).collapse('show');
+)};
